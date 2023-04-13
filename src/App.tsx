dce72279a19ea7eba54dbf907307c5e9cf096597
vkRadchenko <vk.rad4enko@gmail.com>
2023-04-13 18:36:37 +0300

    feat: added component Header, Main, Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,18 @@
-function App() {
-  return <h1>Hello</h1>
+import { AppStyles, PageWrapper } from 'app.styled'
+import Main from './layout/main'
+import Header from 'features/header/header'
+import Footer from 'components/blocks/footer/footer'
+
+function App(): JSX.Element {
+  return (
+    <>
+      <AppStyles />
+      <PageWrapper>
+        <Header />
+        <Main />
+        <Footer />
+      </PageWrapper>
+    </>
+  )
 }
 export default App
