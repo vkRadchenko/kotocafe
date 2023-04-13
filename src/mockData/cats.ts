2c613bb39f096ecd5225069ead8c9eb9475a1763
vkRadchenko <vk.rad4enko@gmail.com>
2023-04-13 18:44:33 +0300

    feat: add dummyData

---
+++ src/mockData/cats.ts
@@ -0,0 +1,89 @@
+const initialCats = [
+  {
+    id: '67rdca3eeb7f6fgeed471815',
+    name: 'Пуша',
+    breed: 'Дворовая',
+    sex: 'Девочка',
+    year: 4,
+    periodInShelter: 1,
+    health: 'good',
+    temper: 'dddddddddddddddddddddddd',
+    qualities: [
+      '67rdca3eeb7f6fgeed471198',
+      '67rdca3eeb7f6fgeed471102',
+      '67rdca3eeb7f6fgeed471100',
+    ],
+    history:
+      'Она грациозна, степенна и как Снежная Королева, кажется такой недоступной. Хотя, чему тут удивляться. Если жить долгое время на улице, где страшно, холодно и так одиноко, волей не волей, станешь холодной и недоступной.',
+  },
+  {
+    id: '67rdca3eeb7f6fgeed471816',
+    name: 'Пуша',
+    breed: 'Дворовая',
+    sex: 'Девочка',
+    year: 4,
+    periodInShelter: 1,
+    health: 'good',
+    temper: 'dddddddddddddddddddddddd',
+    qualities: [
+      '67rdca3eeb7f6fgeed471198',
+      '67rdca3eeb7f6fgeed471102',
+      '67rdca3eeb7f6fgeed471100',
+    ],
+    history:
+      'Она грациозна, степенна и как Снежная Королева, кажется такой недоступной. Хотя, чему тут удивляться. Если жить долгое время на улице, где страшно, холодно и так одиноко, волей не волей, станешь холодной и недоступной.',
+  },
+  {
+    id: '67rdca3eeb7f6fgeed471817',
+    name: 'Пуша',
+    breed: 'Дворовая',
+    sex: 'Девочка',
+    year: 4,
+    periodInShelter: 1,
+    health: 'good',
+    temper: 'dddddddddddddddddddddddd',
+    qualities: [
+      '67rdca3eeb7f6fgeed471198',
+      '67rdca3eeb7f6fgeed471102',
+      '67rdca3eeb7f6fgeed471100',
+    ],
+    history:
+      'Она грациозна, степенна и как Снежная Королева, кажется такой недоступной. Хотя, чему тут удивляться. Если жить долгое время на улице, где страшно, холодно и так одиноко, волей не волей, станешь холодной и недоступной.',
+  },
+  {
+    id: '67rdca3eeb7f6fgeed471818',
+    name: 'Пуша',
+    breed: 'Дворовая',
+    sex: 'Девочка',
+    year: 4,
+    periodInShelter: 1,
+    health: 'good',
+    temper: 'dddddddddddddddddddddddd',
+    qualities: [
+      '67rdca3eeb7f6fgeed471198',
+      '67rdca3eeb7f6fgeed471102',
+      '67rdca3eeb7f6fgeed471100',
+    ],
+    history:
+      'Она грациозна, степенна и как Снежная Королева, кажется такой недоступной. Хотя, чему тут удивляться. Если жить долгое время на улице, где страшно, холодно и так одиноко, волей не волей, станешь холодной и недоступной.',
+  },
+  {
+    id: '67rdca3eeb7f6fgeed471819',
+    name: 'Пуша',
+    breed: 'Дворовая',
+    sex: 'Девочка',
+    year: 4,
+    periodInShelter: 1,
+    health: 'good',
+    temper: 'dddddddddddddddddddddddd',
+    qualities: [
+      '67rdca3eeb7f6fgeed471198',
+      '67rdca3eeb7f6fgeed471102',
+      '67rdca3eeb7f6fgeed471100',
+    ],
+    history:
+      'Она грациозна, степенна и как Снежная Королева, кажется такой недоступной. Хотя, чему тут удивляться. Если жить долгое время на улице, где страшно, холодно и так одиноко, волей не волей, станешь холодной и недоступной.',
+  },
+]
+
+export default initialCats
