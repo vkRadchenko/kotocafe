b2462cc0fd5a18f056712d970dd1b870faaf347f
vkRadchenko <vk.rad4enko@gmail.com>
2023-04-13 18:33:39 +0300

    feat: add base style

---
+++ src/app.styled.ts
@@ -0,0 +1,67 @@
+import styled, { createGlobalStyle } from 'styled-components'
+import colors from 'consts/colors'
+
+export const AppStyles = createGlobalStyle`
+body{
+  -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    margin: 0;
+    font-family: 'Montserrat', sans-serif;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: normal;
+    color: #5e5873;
+    min-width: 320px;
+}
+
+*,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+  }
+  a {
+    color: inherit;
+    text-decoration: inherit;
+    color: ${colors.primary};
+  }
+  ul, li {
+    list-style: none;
+  }
+  img {
+    display: block;
+    max-width: 100%;
+    height: auto;
+  }
+  tr,
+  td,
+  th,
+  p,
+  button,
+  ul,
+  li,
+  h1, h2, h3, h4, h5, h6 {
+    margin: 0;
+    padding: 0;
+  }
+  h1, .h1 {
+    font-size: 26px;
+  }
+  h2, .h2 {
+    font-size: 22px;
+  }
+  h3, .h3 {
+    font-size: 18px;
+  }
+  h4, .h4 {
+    font-size: 16px;
+  }
+  p, li {
+    line-height: 21px;
+  }
+`
+
+export const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`
