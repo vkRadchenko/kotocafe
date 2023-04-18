3e21231337f2db5d98834034c3f76b7b37566d18
vkRadchenko <vk.rad4enko@gmail.com>
2023-04-18 10:54:52 +0300

    feat: install bootstrap-icon

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
