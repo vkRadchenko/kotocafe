df4534245de37e9f35b3d47c06eda7625fa856cd
vkRadchenko <vk.rad4enko@gmail.com>
2023-04-18 11:16:42 +0300

    feat: add Like functional, and style

--- src/components/common/cardCat/cardCat.tsx
+++ src/components/common/cardCat/cardCat.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { CardImage } from './styled'
+import { CardImage, CardWrapper, Like } from './styled'
 
 interface I_CatCardProps {
   id: string
@@ -18,26 +18,51 @@ const CardCat: React.FC<I_CatCardProps> = ({
   name,
   history,
   periodInShelter,
+  sex,
 }) => {
+  const [favorits, setFavorits] = useState(false)
+
+  const handleHeartClick = () => {
+    setFavorits((prevState) => !prevState)
+  }
+
   return (
     <>
-      <div className="col">
-        <div className="card">
+      <div className="col ">
+        <CardWrapper className="card h-100 position-relative">
           <CardImage
             src={require('../../../img/base_87716f252d.jpg')}
             className="card-img-top"
             alt="..."
           />
-          <div className="card-body">
-            <h5 className="card-title">{name}</h5>
-            <p className="card-text">{history}</p>
+          <Like
+            onClick={handleHeartClick}
+            className={`bi ${
+              !favorits ? 'bi-heart' : 'bi-heart-fill text-danger'
+            } fs-3 pe-2`}
+          />
+          <div
+            className="card-body overflow-y-hidden"
+            style={{ height: '200px' }}
+          >
+            <div className="d-flex justify-content-between align-items-center mb-2">
+              <h5 className="">{name}</h5>
+              <span className="d-block text-secondary">{sex}</span>
+            </div>
+
+            <p className="card-text ">{history}</p>
           </div>
-          <div className="card-footer">
+          <div className="ps-3 mb-2 mt-2">
+            <a href="/" className="card-link text-primary">
+              Подробнее
+            </a>
+          </div>
+          <div className="card-footer bg-white">
             <small className="text-body-secondary">
               Находится в приюте {periodInShelter} год
             </small>
           </div>
-        </div>
+        </CardWrapper>
       </div>
     </>
   )
--- src/components/common/cardCat/styled.ts
+++ src/components/common/cardCat/styled.ts
@@ -1,6 +1,23 @@
 import styled from 'styled-components'
 
+export const CardWrapper = styled.div`
+  cursor: pointer;
+  :hover {
+    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  }
+  transition: 0.3s;
+`
+
 export const CardImage = styled.img`
   width: 100%;
   object-fit: scale-down;
+`
+export const Like = styled.i`
+  position: absolute;
+  top: 0;
+  right: 0;
+  cursor: pointer;
+  :hover {
+    color: #e63030;
+  }
 `
