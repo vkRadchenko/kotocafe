24129f8041e6e2d1ee23884993577d7f70020e9c
vkRadchenko <vk.rad4enko@gmail.com>
2023-05-11 19:38:16 +0300

    feat: add functional

--- src/components/blocks/filter/breedFilter.tsx
+++ src/components/blocks/filter/breedFilter.tsx
@@ -1,15 +1,27 @@
-const BreedFilter: React.FC = () => {
+const breedCats = [
+  { key: 'Дворовая', name: 'Дворовая' },
+  { key: 'Сфинкс', name: 'Сфинкс' },
+]
+
+type breedFilter = {
+  onBreedChange: (params: string) => void
+}
+const BreedFilter: React.FC<breedFilter> = ({ onBreedChange }) => {
   return (
     <div className="col mt-3">
       <p className="mb-2 fw-bold">Выберите породу</p>
       <select
         className="form-select form-select-sm"
         aria-label="Default select example"
-        defaultValue={'DEFAULT'}
+        defaultValue={''}
+        onChange={(e) => onBreedChange(e.target.value)}
       >
-        <option value={'DEFAULT'}>Выберите породу</option>
-        <option value="1">Британская</option>
-        <option value="2">Сфинкс</option>
+        <option value={''}>Любая порода</option>
+        {breedCats.map((breed) => (
+          <option key={'breed-' + breed.name} value={breed.key}>
+            {breed.name}
+          </option>
+        ))}
       </select>
     </div>
   )
