9902c23a404fd67b3a1dd636bfc7572c0d549333
vkRadchenko <vk.rad4enko@gmail.com>
2023-05-12 14:47:26 +0300

    feat: add logic clear and start defaultValue

--- src/components/blocks/filter/breedFilter.tsx
+++ src/components/blocks/filter/breedFilter.tsx
@@ -1,3 +1,6 @@
+import Button from 'components/ui/button/button'
+import { useEffect, useState } from 'react'
+
 const breedCats = [
   { key: 'Дворовая', name: 'Дворовая' },
   { key: 'Сфинкс', name: 'Сфинкс' },
@@ -5,16 +8,21 @@ const breedCats = [
 
 type breedFilter = {
   onBreedChange: (params: string) => void
+  clearFilter: string
 }
-const BreedFilter: React.FC<breedFilter> = ({ onBreedChange }) => {
+const BreedFilter: React.FC<breedFilter> = ({ onBreedChange, clearFilter }) => {
+  const handleBreedValue = (params: string) => {
+    onBreedChange(params)
+  }
+
   return (
     <div className="col mt-3">
       <p className="mb-2 fw-bold">Выберите породу</p>
       <select
         className="form-select form-select-sm"
         aria-label="Default select example"
-        defaultValue={''}
-        onChange={(e) => onBreedChange(e.target.value)}
+        onChange={(e) => handleBreedValue(e.target.value)}
+        value={clearFilter}
       >
         <option value={''}>Любая порода</option>
         {breedCats.map((breed) => (
