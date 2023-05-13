f21b3886b6cb1399ea3d3aa7b12edca8cd0dc448
vkRadchenko <vk.rad4enko@gmail.com>
2023-05-13 11:02:59 +0300

    feat: added routing in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { AppStyles, PageWrapper } from 'app.styled'
-import Main from './layout/main'
+import Main from './layout/main/main'
+import Helping from 'layout/help/helping'
+import NotFoundPage from 'layout/404/notFoundPage'
+import Contact from 'layout/contact/contact'
 import Header from 'features/header/header'
 import Footer from 'components/blocks/footer/footer'
+import { Routes, Route } from 'react-router-dom'
+import CatDetalPage from 'components/pages/catDetalPage/catDetalPage'
 
 function App(): JSX.Element {
   return (
@@ -9,7 +14,13 @@ function App(): JSX.Element {
       <AppStyles />
       <PageWrapper>
         <Header />
-        <Main />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/cat/:id" element={<CatDetalPage />} />
+          <Route path="/helping" element={<Helping />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
         <Footer />
       </PageWrapper>
     </>
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import App from './App'
-
+import { BrowserRouter } from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<App />)
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
