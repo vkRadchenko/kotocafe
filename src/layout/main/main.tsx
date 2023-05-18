c29e2b349117cb362354c84da0137f54a9e0ef24
vkRadchenko <vk.rad4enko@gmail.com>
2023-05-18 09:56:40 +0300

    feat: add main page

--- src/layout/main/main.tsx
+++ src/layout/main/main.tsx
@@ -1,17 +1,13 @@
-import { MainWrapper } from './styled'
-import CatsListPage from 'components/pages/catsListPage/catsListPage'
+import { MainWraper } from './styled'
 
-const Main: React.FC = () => (
-  <MainWrapper>
-    <div className="container">
-      <div className="row mt-5 mb-5">
-        <h1 className="mb-4 fw-bold">Наши животные</h1>
-        <h3 className="mb-3 fw-bold">Фильтр</h3>
-        <div className="col-12">
-          <CatsListPage />
-        </div>
-      </div>
-    </div>
-  </MainWrapper>
-)
+const Main = () => {
+  return (
+    <>
+      <MainWraper className="container">
+        <h1>Main</h1>
+      </MainWraper>
+    </>
+  )
+}
+
 export default Main
--- src/layout/main/styled.tsx
+++ src/layout/main/styled.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
 
-export const MainWrapper = styled.div`
+export const MainWraper = styled.div`
   flex-grow: 1;
 `
