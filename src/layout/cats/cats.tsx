e304d4f018ec69c7d9133879575640d41aabdf63
vkRadchenko <vk.rad4enko@gmail.com>
2023-05-18 11:16:52 +0300

    feat: add conditional rendering by catId

--- src/layout/cats/cats.tsx
+++ src/layout/cats/cats.tsx
@@ -1,17 +1,30 @@
+import { useParams } from 'react-router-dom'
 import { CatsWrapper } from './styled'
 import CatsListPage from 'components/pages/catsListPage/catsListPage'
+import CatDetalPage from 'components/pages/catDetalPage/catDetalPage'
 
-const Cats: React.FC = () => (
-  <CatsWrapper>
-    <div className="container">
-      <div className="row mt-5 mb-5">
-        <h1 className="mb-4 fw-bold">Наши животные</h1>
-        <h3 className="mb-3 fw-bold">Фильтр</h3>
-        <div className="col-12">
-          <CatsListPage />
-        </div>
-      </div>
-    </div>
-  </CatsWrapper>
-)
+const Cats: React.FC = () => {
+  const params = useParams()
+  const { catId } = params
+
+  return (
+    <>
+      {catId ? (
+        <CatDetalPage catId={catId} />
+      ) : (
+        <CatsWrapper>
+          <div className="container">
+            <div className="row mt-5 mb-5">
+              <h1 className="mb-4 fw-bold">Наши животные</h1>
+              <h3 className="mb-3 fw-bold">Фильтр</h3>
+              <div className="col-12">
+                <CatsListPage />
+              </div>
+            </div>
+          </div>
+        </CatsWrapper>
+      )}
+    </>
+  )
+}
 export default Cats
