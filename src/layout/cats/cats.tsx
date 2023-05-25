20569cdf4403c660cf35223a7e269d384b5f8e83
vkRadchenko <vk.rad4enko@gmail.com>
2023-05-25 16:23:33 +0300

    feat:  wrapped CatProvider

--- src/layout/cats/cats.tsx
+++ src/layout/cats/cats.tsx
@@ -3,6 +3,7 @@ import { CatsWrapper } from './styled'
 import CatsListPage from 'components/pages/catsListPage/catsListPage'
 import CatDetalPage from 'components/pages/catDetalPage/catDetalPage'
 import EditCatPage from 'components/pages/editCatPage/editCatPage'
+import { CatProvider } from 'hooks/useCat'
 
 const Cats: React.FC = () => {
   const params = useParams()
@@ -10,25 +11,27 @@ const Cats: React.FC = () => {
 
   return (
     <>
-      {catId ? (
-        edit ? (
-          <EditCatPage />
+      <CatProvider>
+        {catId ? (
+          edit ? (
+            <EditCatPage />
+          ) : (
+            <CatDetalPage catId={catId} />
+          )
         ) : (
-          <CatDetalPage catId={catId} />
-        )
-      ) : (
-        <CatsWrapper>
-          <div className="container">
-            <div className="row mt-5 mb-5">
-              <h1 className="mb-4 fw-bold">Наши животные</h1>
-              <h3 className="mb-3 fw-bold">Фильтр</h3>
-              <div className="col-12">
-                <CatsListPage />
+          <CatsWrapper>
+            <div className="container">
+              <div className="row mt-5 mb-5">
+                <h1 className="mb-4 fw-bold">Наши животные</h1>
+                <h3 className="mb-3 fw-bold">Фильтр</h3>
+                <div className="col-12">
+                  <CatsListPage />
+                </div>
               </div>
             </div>
-          </div>
-        </CatsWrapper>
-      )}
+          </CatsWrapper>
+        )}
+      </CatProvider>
     </>
   )
 }
