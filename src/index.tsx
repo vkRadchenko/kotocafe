050747d7d2bb32d68dd62fe9a454a01dde70c61b
vkRadchenko <vk.rad4enko@gmail.com>
2023-06-08 20:16:21 +0300

    feat: create store and add Provider to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,16 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import { createStore } from 'store/createStore'
+import { Provider } from 'react-redux'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+const store = createStore()
+
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 )
