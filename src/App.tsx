0e8fbcc5e4919ccd7f5c03b88607590494b9b9e9
vkRadchenko <vk.rad4enko@gmail.com>
2023-06-12 11:30:47 +0300

    feat: add dispatch. Add loadQualitiesList. Delete QualitiesProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { AppStyles, PageWrapper } from 'app.styled'
 import Main from './layout/main/main'
 import NotFoundPage from 'layout/404/notFoundPage'
@@ -12,32 +13,37 @@ import AuthProvider from 'hooks/useAuth'
 import LogOut from 'layout/logOut/logOut'
 import CreateCat from 'layout/createCat/createCat'
 import { CatProvider } from 'hooks/useCat'
-import { QualitiesProvider } from 'hooks/useQualities'
 import UserDetalPage from 'components/pages/userDetalPage/userDetalPage'
+import { useDispatch } from 'react-redux'
+import { loadQualitiesList } from 'store/qualities'
 
 function App(): JSX.Element {
+  const dispatch: any = useDispatch()
+
+  useEffect(() => {
+    dispatch(loadQualitiesList())
+  }, [dispatch])
+
   return (
     <>
       <AppStyles />
       <PageWrapper>
         <AuthProvider>
           <CatProvider>
-            <QualitiesProvider>
-              <Header />
-              <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="/cats/:catId?/:edit?" element={<Cats />} />
-                <Route
-                  path="/users/:userId?/:edit?"
-                  element={<UserDetalPage />}
-                />
-                <Route path="/additem" element={<CreateCat />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/login/:type?" element={<Login />} />
-                <Route path="/logout" element={<LogOut />} />
-                <Route path="*" element={<NotFoundPage />} />
-              </Routes>
-            </QualitiesProvider>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Main />} />
+              <Route path="/cats/:catId?/:edit?" element={<Cats />} />
+              <Route
+                path="/users/:userId?/:edit?"
+                element={<UserDetalPage />}
+              />
+              <Route path="/additem" element={<CreateCat />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/login/:type?" element={<Login />} />
+              <Route path="/logout" element={<LogOut />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
           </CatProvider>
         </AuthProvider>
         <Footer />
