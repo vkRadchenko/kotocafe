b62c7b325856ecf7542b602dc7505ecab45ef084
vkRadchenko <vk.rad4enko@gmail.com>
2023-06-20 14:30:24 +0300

    feat: combineReducers added breedReducer  and catsReducer

--- src/store/createStore.ts
+++ src/store/createStore.ts
@@ -1,7 +1,15 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import qualitiesReducer from './qualities'
+import catsReducer from './cats'
+import userReducer from './user'
+import breedReducer from './breed'
 
-const rootReducer = combineReducers({ qualities: qualitiesReducer })
+const rootReducer = combineReducers({
+  qualities: qualitiesReducer,
+  breed: breedReducer,
+  cats: catsReducer,
+  user: userReducer,
+})
 
 export function createStore() {
   return configureStore({
