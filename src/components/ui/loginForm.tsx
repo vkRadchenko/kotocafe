e5161d988b5773427a4505457ab6908312d53dd4
vkRadchenko <vk.rad4enko@gmail.com>
2023-06-20 15:11:36 +0300

    feat: change loginForm. LogIn method useAuth =>  useDispatch

--- src/components/ui/loginForm.tsx
+++ src/components/ui/loginForm.tsx
@@ -1,31 +1,27 @@
 import React, { useState, useEffect } from 'react'
-import { validator, ValidatorConfig } from 'utils/validator'
+import { validator } from 'utils/validator'
 import TextField from 'components/common/form/textField'
-import CheckBoxField from 'components/common/form/checkBoxField'
-import { useAuth } from 'hooks/useAuth'
-import { useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { getAuthErrors, logIn } from 'store/user'
 
 const LoginForm: React.FC = () => {
-  const navigate = useNavigate()
   const [data, setData] = useState({
     email: '',
     password: '',
-    stayOn: false,
   })
-  const { logIn }: any = useAuth()
+  const dispatch: any = useDispatch()
+  const loginError = useSelector(getAuthErrors())
 
   const [errors, setErrors] = useState<{ [fieldName: string]: string }>({
     email: '',
     password: '',
   })
-  const [enterError, setEnterError]: any = useState(null)
 
   const handleChange = (target: { name: string; value: string | boolean }) => {
     setData((prevState) => ({
       ...prevState,
       [target.name]: target.value,
     }))
-    setEnterError(null)
   }
 
   const validatorConfig = {
@@ -56,17 +52,11 @@ const LoginForm: React.FC = () => {
 
   const isValid = Object.values(errors).every((str) => str === '')
 
-  const handleSubmit = async (e: React.SyntheticEvent) => {
+  const handleSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault()
     const isValid = validate()
     if (!isValid) return
-    try {
-      await logIn(data)
-      navigate('/')
-    } catch (error: any) {
-      setEnterError(error.message)
-      console.log(error.message)
-    }
+    dispatch(logIn(data))
   }
   return (
     <form onSubmit={handleSubmit}>
@@ -85,16 +75,13 @@ const LoginForm: React.FC = () => {
         onChange={handleChange}
         error={errors.password}
       />
-      <CheckBoxField value={data.stayOn} onChange={handleChange} name="stayOn">
-        Оставаться в системе
-      </CheckBoxField>
-      {enterError && (
-        <p className="text-danger mb-1 text-center">{enterError}</p>
+      {loginError && (
+        <p className="text-danger mb-1 text-center">{loginError}</p>
       )}
       <button
         className="btn btn-primary w-100 mx-auto"
         type="submit"
-        disabled={!isValid || enterError}
+        disabled={!isValid}
       >
         Submit
       </button>
