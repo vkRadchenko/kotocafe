98a8e573858b9f4d0015c1e5c1d1845ea2f10baa
vkRadchenko <vk.rad4enko@gmail.com>
2023-06-20 15:03:09 +0300

    feat: remove Providers and dispatch. Add AppLoader

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react'
 import { AppStyles, PageWrapper } from 'app.styled'
 import Main from './layout/main/main'
 import NotFoundPage from 'layout/404/notFoundPage'
@@ -9,46 +8,32 @@ import { Routes, Route } from 'react-router-dom'
 import Cats from './layout/cats/cats'
 import Login from 'layout/login/login'
 import { ToastContainer } from 'react-toastify'
-import AuthProvider from 'hooks/useAuth'
 import LogOut from 'layout/logOut/logOut'
 import CreateCat from 'layout/createCat/createCat'
-import { CatProvider } from 'hooks/useCat'
 import UserDetalPage from 'components/pages/userDetalPage/userDetalPage'
-import { useDispatch } from 'react-redux'
-import { loadQualitiesList } from 'store/qualities'
+import AppLoader from 'components/ui/hoc/appLoader'
 
 function App(): JSX.Element {
-  const dispatch: any = useDispatch()
-
-  useEffect(() => {
-    dispatch(loadQualitiesList())
-  }, [dispatch])
-
   return (
     <>
-      <AppStyles />
-      <PageWrapper>
-        <AuthProvider>
-          <CatProvider>
-            <Header />
-            <Routes>
-              <Route path="/" element={<Main />} />
-              <Route path="/cats/:catId?/:edit?" element={<Cats />} />
-              <Route
-                path="/users/:userId?/:edit?"
-                element={<UserDetalPage />}
-              />
-              <Route path="/additem" element={<CreateCat />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/login/:type?" element={<Login />} />
-              <Route path="/logout" element={<LogOut />} />
-              <Route path="*" element={<NotFoundPage />} />
-            </Routes>
-          </CatProvider>
-        </AuthProvider>
-        <Footer />
-      </PageWrapper>
-      <ToastContainer />
+      <AppLoader>
+        <AppStyles />
+        <PageWrapper>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/cats/:catId?/:edit?" element={<Cats />} />
+            <Route path="/users/:userId?/:edit?" element={<UserDetalPage />} />
+            <Route path="/additem" element={<CreateCat />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/login/:type?" element={<Login />} />
+            <Route path="/logout" element={<LogOut />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+          <Footer />
+        </PageWrapper>
+        <ToastContainer />
+      </AppLoader>
     </>
   )
 }
