e290c09faf326cf88f515464a7bbb9db40775bd9
vkRadchenko <vk.rad4enko@gmail.com>
2023-07-05 12:32:51 +0300

    feat: I set up the environment. Installed nodemon and cross-env. Create folder config and create fail default config.

--- server/app.js
+++ server/app.js
@@ -1,1 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+const chalk = require('chalk')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+const PORT = config.get('port') ?? 8080
+
+if (process.env.NODE_ENV === 'production') {
+  console.log('production')
+} else {
+  console.log('development')
+}
+
+app.listen(PORT, () => {
+  console.log(chalk.green(`Server has been starter on port ${PORT}`))
+})
