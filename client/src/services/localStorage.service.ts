3be1076a69a79bcdca961b372ab03871d26eaeee
vkRadchenko <vk.rad4enko@gmail.com>
2023-08-08 12:34:03 +0300

    refactor: change localStorage

--- client/src/services/localStorage.service.ts
+++ client/src/services/localStorage.service.ts
@@ -1,24 +1,24 @@
 const TOKEN_KEY = 'jwt-token'
 const REFRESH_KEY = 'jwt-refresh-token'
-const EXPIRES_KEY = 'jwt-expires-token'
-const USERID_KEY = 'user-local-token'
+const EXPIRES_KEY = 'jwt-expires'
+const USERID_KEY = 'user-local-id'
 
 export function setTokens({
   refreshToken,
-  idToken,
-  localId,
+  accsessToken,
+  userId,
   expiresIn = 3600,
 }: {
   refreshToken: string
-  idToken: string
-  localId: string
-  expiresIn?: number
+  accsessToken: string
+  userId: string
+  expiresIn: any
 }) {
-  const expiresDate = new Date().getTime() + Number(expiresIn) * 1000
-  localStorage.setItem(USERID_KEY, localId)
-  localStorage.setItem(TOKEN_KEY, idToken)
+  const expiresDate: any = new Date().getTime() + expiresIn * 1000
+  localStorage.setItem(USERID_KEY, userId)
+  localStorage.setItem(TOKEN_KEY, accsessToken)
   localStorage.setItem(REFRESH_KEY, refreshToken)
-  localStorage.setItem(EXPIRES_KEY, expiresDate.toString())
+  localStorage.setItem(EXPIRES_KEY, expiresDate)
 }
 
 export function getAccessToken() {
