59023af43c38100b61d5b465a761f600059f13ed
vkRadchenko <vk.rad4enko@gmail.com>
2023-08-08 12:33:29 +0300

    refactor: change interceptors.  Add authorization and access token

--- client/src/services/http.servise.ts
+++ client/src/services/http.servise.ts
@@ -18,29 +18,43 @@ function transformData(data: any) {
 
 http.interceptors.request.use(
   async function (config: any) {
+    const expiresDate = localStorageService.getTokenExpiresDate()
+    const refreshToken = localStorageService.getRefreshToken()
+    const isExpired = refreshToken && Number(expiresDate) < Date.now()
+
     if (configFile.isFireBase) {
       const containSlash = /\/$/gi.test(config.url)
       config.url =
         (containSlash ? config.url.slice(0, -1) : config.url) + '.json'
 
-      const expiresDate = localStorageService.getTokenExpiresDate()
-      const refreshToken = localStorageService.getRefreshToken()
-      if (refreshToken && Number(expiresDate) < Date.now()) {
+      if (isExpired) {
         const data = await authService.refresh()
 
         localStorageService.setTokens({
           refreshToken: data.refresh_token,
-          idToken: data.id_token,
+          accsessToken: data.id_token,
           expiresIn: data.expires_in,
-          localId: data.user_id,
+          userId: data.user_id,
         })
       }
       const accessToken = localStorageService.getAccessToken()
       if (accessToken) {
         config.params = { ...config.params, auth: accessToken }
       }
-    }
+    } else {
+      if (refreshToken && Number(expiresDate) < Date.now()) {
+        const data = await authService.refresh()
+        localStorageService.setTokens(data)
+      }
+      const accessToken = localStorageService.getAccessToken()
 
+      if (accessToken) {
+        config.headers = {
+          ...config.headers,
+          Authorization: `Bearer ${accessToken}`,
+        }
+      }
+    }
     return config
   },
   function (error) {
@@ -50,7 +64,11 @@ http.interceptors.request.use(
 
 http.interceptors.response.use(
   (res) => {
-    res.data = { content: transformData(res.data) }
+    if (configFile.isFireBase) {
+      res.data = { content: transformData(res.data) }
+    }
+    res.data = { content: res.data }
+
     return res
   },
   function (error) {
