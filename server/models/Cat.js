41415413ac7b98bca90005775146d387e5bc72a5
vkRadchenko <vk.rad4enko@gmail.com>
2023-08-08 12:13:10 +0300

    refactor: change cat model

--- server/models/Cat.js
+++ server/models/Cat.js
@@ -4,8 +4,8 @@ const shema = new Schema(
   {
     name: { type: String },
     sex: { type: String, enum: ['Кошка', 'Кот'] },
-    breed: { type: Schema.Types.ObjectId, ref: 'Breed' },
-    quality: [{ type: Schema.Types.ObjectId, ref: 'Quality' }],
+    breed: String,
+    qualities: [{ type: Schema.Types.ObjectId, ref: 'Quality' }],
     userId: { type: Schema.Types.ObjectId, ref: 'User' },
     image: String,
     age: String,
